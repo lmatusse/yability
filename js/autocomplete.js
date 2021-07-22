f9f6c5553c368e372049bf867c5869c427420b80
lmatusse <englmatusse@gmail.com>
2021-07-22 23:18:40 +0200

    Subindo o projecto

---
+++ js/autocomplete.js
@@ -0,0 +1,22 @@
+$(function(){
+    var availableTags = [
+   "Laranja",
+   "Banana"
+    ];
+    
+    $(document).on('focus','.descri', function(){
+       // este if verifica se o campo já tem autocomplete
+       if( !$(this).hasClass("ui-autocomplete-input") ){
+          $(this).autocomplete({
+             source: availableTags
+        });
+       }
+    });
+    
+    $("#addicionar").on("click", function(){
+       
+       $("#box").append('<input class="descri">');
+       
+    });
+    
+ });
